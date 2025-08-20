16aa1dab91ae6b9c69df0bd24cd58cb619a354c5
Kaique Biazon <kaiquebiazon1@gmail.com>
2025-08-20 19:26:46 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,43 @@
-const btnAvancar = document.getElementById("btn-avancar");
-const btnVoltar = document.getElementById("btn-voltar");
+const botoesAvancar = document.getElementById("btn-avancar");
+const botoesVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
-
 let cartaoAtual = 0;
 
+// Função para esconder o cartão selecionado
 function esconderCartaoSelecionado() {
   const cartaoSelecionado = document.querySelector(".selecionado");
   cartaoSelecionado.classList.remove("selecionado");
 }
 
-function mostrarCartao(indice) {
-  cartoes[indice].classList.add("selecionado");
+// Função para mostrar o próximo cartão
+function mostrarCartao(indiceCartao) {
+  cartoes[indiceCartao].classList.add("selecionado");
 }
 
-btnAvancar.addEventListener("click", function () {
+// Botão avançar
+botoesAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return;
+
   esconderCartaoSelecionado();
   cartaoAtual++;
   mostrarCartao(cartaoAtual);
 });
 
-btnVoltar.addEventListener("click", function () {
+// Botão voltar
+botoesVoltar.addEventListener("click", function () {
   if (cartaoAtual === 0) return;
+
   esconderCartaoSelecionado();
   cartaoAtual--;
   mostrarCartao(cartaoAtual);
+});
+
+// ----------------- VIRAR CARTA -----------------
+cartoes.forEach((cartao) => {
+  const verso = cartao.querySelector(".carta-virada");
+
+  cartao.addEventListener("click", () => {
+    verso.classList.toggle("mostrar-fundo-carta");
+  });
 });
 
